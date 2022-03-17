099b0e36002a7decdafe4b97817c57b76e6efa69
xygxue <yugexue@gmail.com>
2022-03-17 14:41:50 +0100

    add latent_space_output.pt

--- clustering.py
+++ clustering.py
@@ -7,12 +7,16 @@ from datasets.dataset import padding_mask
 from models.ts_transformer import TSTransformerEncoder, TSTransformerEncoderOutput
 from utils.utils import load_model
 
+sample_size = 4500
+batch_size = 32
+max_seq_len = 675
+feat_dim = 12
 
-config_path = 'experiments/A0000008261_pretrained_2022-03-11_19-15-21_jwM/configuration.json'
-model_path = 'experiments/A0000008261_pretrained_2022-03-11_19-15-21_jwM/checkpoints/model_best.pth'
-best_pred_path = 'experiments/A0000008261_pretrained_2022-03-11_19-15-21_jwM/predictions/best_predictions.npz'
+config_path = 'experiments/3_pretrained_2022-03-16_19-45-34_ay2/configuration.json'
+model_path = 'experiments/3_pretrained_2022-03-16_19-45-34_ay2/checkpoints/model_best.pth'
+best_pred_path = 'experiments/3_pretrained_2022-03-16_19-45-34_ay2/predictions/best_predictions.npz'
 train_csv_path = 'data_exp/TRAIN.csv'
-
+latent_space_path = 'experiments/3_pretrained_2022-03-16_19-45-34_ay2/predictions/latent_space_output.pt'
 train = pd.read_csv(train_csv_path, index_col='account_id')
 
 
@@ -24,28 +28,34 @@ for k, v in trans_acc_grp_unnorm:
     # append resized array to list
     features.append(array)
 
-X = torch.zeros(4500, 675, 12)  # (batch_size, padded_length, feat_dim)
+X = torch.zeros(sample_size, max_seq_len, feat_dim)  # (sample_size, padded_length, feat_dim)
 lengths = [a.shape[0] for a in features]
 
 
-for i in range(4500):
-    end = min(lengths[i], 675)
+for i in range(sample_size):
+    end = min(lengths[i], max_seq_len)
     X[i, :end, :] = features[i][:end, :]
 
-X_batch = torch.split(X, 30)
+X_batch = torch.split(X, batch_size) # (batch_size, padded_length, feat_dim)
 
-pad = padding_mask(torch.tensor(lengths, dtype=torch.int16), max_len=675)
+pad = padding_mask(torch.tensor(lengths, dtype=torch.int16), max_len=max_seq_len)
+pad_batch = torch.split(pad, batch_size)
 
 with open(config_path, "r") as config_file:
     config = json.loads(config_file.read())
 
-model = TSTransformerEncoderOutput(12, 675, config['d_model'], config['num_heads'],
+model = TSTransformerEncoderOutput(feat_dim, max_seq_len, config['d_model'], config['num_heads'],
                              config['num_layers'], config['dim_feedforward'], dropout=config['dropout'],
                              pos_encoding=config['pos_encoding'], activation=config['activation'],
                              norm=config['normalization_layer'], freeze=config['freeze'])
-pre_model = load_model(model, model_path)
+pre_model = load_model(model.to('cuda'), model_path)
 model_train = model.train()
 output = []
-for batch in X_batch:
-    output_batch = model_train(batch.to('cpu'), pad)
-    output = torch.cat((output, output_batch), 0)
+
+for i in range(len(X_batch)):
+    try:
+        output_batch = model_train(X_batch[i].to('cuda'), pad_batch[i].to('cuda'))
+        output = torch.cat((output, output_batch), dim=1)
+    except:
+        output = output_batch
+torch.save(output, latent_space_path)
